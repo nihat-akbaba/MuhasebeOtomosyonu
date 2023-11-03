720f64028898642ebeb16547f1236d95c57d3069
nihat-akbaba <74313118+nihat-akbaba@users.noreply.github.com>
2023-11-03 15:29:03 +0300

    MuhasebeOtomosyonu
    Muhasebe için geliştirilmiştir

---
+++ Nihat akbaba/OnMuhasebeOtomasyonu/iletisim.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OnMuhasebeOtomasyonu
+{
+    public partial class iletisim : Form
+    {
+        public iletisim()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
